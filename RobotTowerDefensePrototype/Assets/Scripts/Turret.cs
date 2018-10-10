abaffd16222f5fda1677163dd0bfe9d337683604
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2018-10-11 06:44:38 +1100

    uploading Unity prototype
    this is the prototype of the game, very lack luster.

---
+++ RobotTowerDefensePrototype/Assets/Scripts/Turret.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Turret : MonoBehaviour {
+    public float Range;
+    public int Damage;
+    public GameObject[] Targets;
+
+    void Start() {
+        FindTargets();
+    }
+
+    void Update() {
+        if (Targets.Length == 0)
+        {
+            FindTargets();
+        }
+        else
+        {
+            TurretActivate();
+        }
+    }
+
+    void FindTargets()
+    {
+        return;
+    }
+    
+    void TurretActivate()
+    {
+        #region lerped lookat?
+        //Vector3 TargetPos = Targets[0].transform.position;
+        //float Angle = Vector3.Angle(transform.position, TargetPos);
+
+        //Debug.Log(Angle.ToString());
+
+        //if (transform.rotation.y == Angle)
+        //{
+        //    return;
+        //}
+        //transform.Rotate(transform.up, (Angle) * Time.deltaTime);
+        #endregion
+
+        transform.LookAt(Targets[0].transform);
+
+
+    }
+}
