4f1e15f9a8831ee723207dfcd62b5d876fc86f53
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2018-10-11 11:20:51 +1100

    CameraControls
    added prototype of camera controls works very well I swear

---
+++ RobotTowerDefensePrototype/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    Transform CamTransform;
+    public GameObject target;
+    bool option = true;
+	void Start () {
+        CamTransform = GetComponent<Transform>();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown("q"))
+        {
+            option = !option;
+            if (option)
+            {
+                CamTransform.transform.position = new Vector3(-4, 9, -14);
+                CamTransform.rotation = new Quaternion(20, 0, 0, 0);
+            }
+            else
+            {
+                CamTransform.rotation = new Quaternion(0, 0, 0, 0);
+                CamTransform.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 5f, target.transform.position.z);
+            }
+        }
+
+        if (option)
+        {
+            CamControls1();
+        }
+        else {
+            CamControls2();
+        }
+    }
+    void CamControls1()
+    {
+        CamTransform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        CamTransform.LookAt(target.transform);
+    }
+    void CamControls2()
+    {
+        CamTransform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"),0));
+    }
+
+}
