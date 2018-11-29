b1df8cbd890a48f62555605059b251cf71de820b
schmawkins <42603720+schmawkins@users.noreply.github.com>
2018-11-29 17:09:33 +1100

    Imported Asset Package
    Imported Asset Package
    - Prefab Missing?  Will reattempt

---
+++ RobotTowerDefensePrototype/Assets/Scripts/Clouds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.left * 4 * Time.deltaTime);
+    }
+}
